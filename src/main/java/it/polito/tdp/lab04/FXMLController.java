f000f64fd6b439f663219265bfd07192e40d606b
Marco Camaggio <100542819+mcamaggio@users.noreply.github.com>
2022-04-16 16:01:08 +0200

    2022/04/16

--- src/main/java/it/polito/tdp/lab04/FXMLController.java
+++ src/main/java/it/polito/tdp/lab04/FXMLController.java
@@ -1,23 +1,240 @@
-/**
- * Sample Skeleton for 'Scene.fxml' Controller Class
- */
-
 package it.polito.tdp.lab04;
 
 import java.net.URL;
+import java.util.Collections;
+import java.util.List;
 import java.util.ResourceBundle;
+
+import it.polito.tdp.lab04.model.Corso;
+import it.polito.tdp.lab04.model.Model;
+import it.polito.tdp.lab04.model.Studente;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class FXMLController {
+	
+	private Model model;
+	private List<Corso> corsi;
 
-    @FXML // ResourceBundle that was given to the FXMLLoader
+    @FXML
     private ResourceBundle resources;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    @FXML
     private URL location;
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
-    void initialize() {
+    @FXML
+    private Button btnCercaCorsi;
+
+    @FXML
+    private Button btnCercaIscrittiCorso;
+
+    @FXML
+    private Button btnCercaNome;
+
+    @FXML
+    private Button btnIscrivi;
+
+    @FXML
+    private Button btnReset;
+
+    @FXML
+    private ComboBox<Corso> cmbCorsi;
+
+    @FXML
+    private TextField txtCognome;
+
+    @FXML
+    private TextArea txtElenco;
+
+    @FXML
+    private TextField txtMatricola;
+
+    @FXML
+    private TextField txtNome;
+
+    @FXML
+    void doCercaCorso(ActionEvent event) {
+    	
+    	txtElenco.clear();
+    	
+    	try {
+    		int matricola = Integer.parseInt(txtMatricola.getText());
+    		Studente studente = model.getStudente(matricola);
+    		
+    		if(studente == null) {
+    			txtMatricola.appendText("Nessun risultato: matricola inesistente");
+    			return;
+    		}
+    		
+    		List<Corso> corsi = model.corsiFromStudente(studente);
+    		StringBuilder sb = new StringBuilder();
+    		
+    		for(Corso corso : corsi) {
+    			sb.append(String.format("%-8s ", corso.getCodins()));
+    			sb.append(String.format("%-4s ", corso.getCrediti()));
+    			sb.append(String.format("%-45s ", corso.getNome()));
+    			sb.append(String.format("%-4s ", corso.getPd()));
+    			sb.append("\n");
+    		}
+    		
+    		txtElenco.appendText(sb.toString());
+    	
+    	} catch (NumberFormatException e) {
+    		txtElenco.setText("Inserire una matricola nel formato corretto");
+    	} catch (RuntimeException e) {
+    		txtElenco.setText("ERRORE DI CONNESSIONE AL DATABASE!");
+    	}
+    	 
+    }
+
+    @FXML
+    void doCercaIscrittiCorso(ActionEvent event) {
+    	
+    	txtElenco.clear();
+    	txtNome.clear();
+    	txtCognome.clear();
+    	txtMatricola.clear();
+    	
+    	try {
+	    	Corso corso = cmbCorsi.getValue();
+	    	if(corso == null) {
+	    		txtElenco.setText("Selezionare un corso.");
+	    		return;
+	    	}
+	    	
+	    	List<Studente> studenti = model.studentiIscrittiAlCorso(corso);
+	    	StringBuilder sb = new StringBuilder();
+	    	
+	    	for(Studente studente : studenti) {
+	    		sb.append(String.format("%-10s ", studente.getMatricola()));
+	    		sb.append(String.format("%-20s ", studente.getCognome()));
+	    		sb.append(String.format("%-20s ", studente.getNome()));
+	    		sb.append(String.format("%-10s ", studente.getCds()));
+	    		sb.append("\n");
+	    	}
+	    	
+	    	txtElenco.appendText(sb.toString());
+    	
+    	} catch (RuntimeException e) {
+    		txtElenco.setText("ERRORE DI CONNESSIONE AL DATABASE!");
+    	}
+    	
+    }
+
+    @FXML
+    void doCercaNome(ActionEvent event) {
+    	
+    	txtElenco.clear();
+    	txtNome.clear();
+    	txtCognome.clear();
+    	
+    	try {	
+	    	int matricola = Integer.parseInt(txtMatricola.getText());
+	    	Studente studente = model.getStudente(matricola);
+	    	
+	    	if(studente == null) {
+	    		txtElenco.setText("Nessun risultato: matricola inesistente");
+	    		return;
+	    	}
+	    	
+	    	txtNome.setText(studente.getNome());
+	    	txtCognome.setText(studente.getCognome());
+	    
+    	} catch (NumberFormatException e) {
+    		txtElenco.setText("Inserire una matricola nel formato corretto");
+	    } catch (RuntimeException e) {
+			txtElenco.setText("ERRORE DI CONNESSIONE AL DATABASE!");
+		}
 
     }
+
+    @FXML
+    void doIscrivi(ActionEvent event) {
+    	
+    	txtElenco.clear();
+    	
+    	try {
+	    	int matricola = Integer.parseInt(txtMatricola.getText());
+	    	Studente studente = model.getStudente(matricola);
+	    	
+	    	Corso corso = cmbCorsi.getValue();
+	    	
+	    	if(studente == null) {
+	    		txtElenco.appendText("Nessun risultato: matricola inesistente");
+	    		return;
+	    	}
+	    	if(corso == null) {
+	    		txtElenco.appendText("Selezionare un corso.");
+	    		return;
+	    	}
+	    	
+	    	txtNome.setText(studente.getNome());
+	    	txtCognome.setText(studente.getCognome());
+	    	
+	    	if(model.isStudenteIscrittoACorso(studente, corso)) {
+	    		txtElenco.appendText("Studente gia iscritto al corso.");
+	    		return;
+	    	}
+	    	
+	    	if(!model.inscriviStudenteACorso(studente, corso)) {
+	    		txtElenco.appendText("Errore durante l'iscrizione al corso.");
+	    		return;
+	    	} else {
+	    		txtElenco.appendText("Studente iscritto al corso");
+	    	}
+	    	
+	    } catch (NumberFormatException e) {
+			txtElenco.setText("Inserire una matricola nel formato corretto");
+	    } catch (RuntimeException e) {
+			txtElenco.setText("ERRORE DI CONNESSIONE AL DATABASE!");
+		}
+
+    }
+
+    @FXML
+    void doReset(ActionEvent event) {
+    	cmbCorsi.setPromptText("Corsi");
+    	txtMatricola.clear();
+    	txtNome.clear();
+    	txtCognome.clear();
+    	txtElenco.clear();
+    	cmbCorsi.getSelectionModel().clearSelection();
+
+    }
+
+    @FXML
+    void initialize() {
+        assert btnCercaCorsi != null : "fx:id=\"btnCercaCorsi\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnCercaIscrittiCorso != null : "fx:id=\"btnCercaIscrittiCorso\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnCercaNome != null : "fx:id=\"btnCercaNome\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnIscrivi != null : "fx:id=\"btnIscrivi\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert cmbCorsi != null : "fx:id=\"cmbCorsi\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtCognome != null : "fx:id=\"txtCognome\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtElenco != null : "fx:id=\"txtElenco\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtMatricola != null : "fx:id=\"txtMatricola\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtNome != null : "fx:id=\"txtNome\" was not injected: check your FXML file 'Scene.fxml'.";
+
+    }
+
+	public void setModel(Model model) {
+		this.model = model;
+		setComboItems();
+		
+	}
+
+	private void setComboItems() {
+		// Ottieni tutti i corsi dal model
+		corsi = model.getTuttiICorsi();
+		
+		// Aggiungi tutti i corsi alla ComboBox
+		Collections.sort(corsi); // Ordine alfabetico per maggior ordine
+		cmbCorsi.getItems().addAll(corsi); // richiama il toString dell'oggetto Corso
+		
+	}
+
 }
--- src/main/java/it/polito/tdp/lab04/model/Studente.java
+++ src/main/java/it/polito/tdp/lab04/model/Studente.java
@@ -1,5 +1,63 @@
 package it.polito.tdp.lab04.model;
 
 public class Studente {
+	
+	private int matricola;
+	private String nome;
+	private String cognome;
+	private String cds;
+	
+	public Studente(int matricola) {
+		this.matricola = matricola;
+	}
+	
+	public Studente(int matricola, String nome, String cognome, String cds) {
+		super();
+		this.matricola = matricola;
+		this.nome = nome;
+		this.cognome = cognome;
+		this.cds = cds;
+	}
+
+	public int getMatricola() {
+		return matricola;
+	}
+
+	public void setMatricola(int matricola) {
+		this.matricola = matricola;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getCds() {
+		return cds;
+	}
+
+	public void setCds(String cds) {
+		this.cds = cds;
+	}
+
+	@Override
+	public String toString() {
+		return cognome + " " + nome;
+	}
+	
+	
+	
+	
 
 }
